45f61b581d4e7e2f0a6fa95d2ee1f833eb079e56
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-05-31 17:22:34 -0400

    added nextauth signin functionality

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
 
+import User from '@models/user';
 import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
@@ -10,20 +11,36 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user.email });
 
-      // check if user already exists
-      
-      // if not, create user
+      session.user.id = sessionUser._id.toString();
 
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
+
+        // if not, create user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(' ', '').toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
   },
 });
 
