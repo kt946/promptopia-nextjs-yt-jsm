819804177e7922bdf4033bc549299e01b2b0fb4c
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-06-01 14:40:12 -0400

    added prompt model, route, and functionality to create prompts

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,22 @@
+import { connectToDB } from '@utils/database';
+import Prompt from '@models/prompt';
+
+export const POST = async (req) => {
+  const { userId, prompt, tag } = await req.json();
+
+  try {
+    // lambda function, called everytime when we want to connect to the database
+    await connectToDB();
+    const newPrompt = new Prompt({
+      creator: userId,
+      prompt,
+      tag,
+    });
+
+    await newPrompt.save();
+
+    return new Response(JSON.stringify(newPrompt), { status: 201 });
+  } catch (error) {
+    return new Response('Failed to create a new prompt', { status: 500 });
+  }
+};
